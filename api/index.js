53a0e77d55f006ad7974c6e8c68c570f40c831c4
adilummar <madilummer@gmail.com>
2025-03-06 19:06:18 +0530

    created a function to handle the error

--- api/index.js
+++ api/index.js
@@ -1,25 +1,35 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 mongoose
-.connect(process.env.MONGO)
-.then(() => {
-  console.log("db connected successfully");
-})
-.catch((err) => {
-  console.log(err);
-});
-app.use(express.json())
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("db connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(express.json());
 const port = 3000;
 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(port, () => {
   console.log(`app is listening port:${port}`);
 });
