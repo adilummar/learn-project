496ee16263891761f2c1135d20e82495fabaf430
adilummar <madilummer@gmail.com>
2025-03-06 18:37:58 +0530

    created sign up api functionality

--- api/index.js
+++ api/index.js
@@ -2,21 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 const app = express();
 mongoose
 .connect(process.env.MONGO)
 .then(() => {
-    console.log("db connected successfully");
+  console.log("db connected successfully");
 })
 .catch((err) => {
-    console.log(err);
+  console.log(err);
 });
+app.use(express.json())
 const port = 3000;
 
 
 app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
 app.listen(port, () => {
   console.log(`app is listening port:${port}`);
 });
